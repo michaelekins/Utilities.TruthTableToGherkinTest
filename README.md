46b14dc07bc96b10f4258a1a5514273ea317a82b
CW-DATA\michael ekins <m.ekins@craneware.com>
2021-01-20 18:23:14 +0000

    initial commit, simple utility to turn truth tables which are easy to build into Gherkin style tests which are easy to read with little effort

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Utilities.TruthTableToGherkinTest
+
+Structure of csv must be:
+> ""Header1","Header2","Header3","Header4","Header5","Header6"
+> "Given1Statement","Given2Statement","Given3Statement",,"Then1Statement","Then2Statement","Then3Statement"
+> "Given1Value","Given2Value","Given3Value",,"Then1Value","Then2Value","Then3Value"
+
+Please note the empty column between **Given** and **Then** statements
+
+example whenStatement (define this in the code):
+>      "When I try to fit the shape in the hole"
+ example csv file:
+>      the shape,the colour,the hole,,shape fits,the colour
+>      a square, red, a square,,true, irrelevant
+>      a square,red,a circle,, false, irrelevant
+>      a square, red, a triangle,,false, irrelevant
+>      ...
+
+ example output:
+>      Given the shape is a square
+>          And the colour is red
+>          And the hole is a square
+>          When I try to fit the shape in the hole
+>          Then the shape fits is true
+>          And the colour is irrelevant
+
---
+++ TruthTableToGherkinTest/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using LumenWorks.Framework.IO.Csv;
+
+// structure of csv must be: 
+// "Given1Statement","Given2Statement","Given3Statement",,"Then1Statement","Then2Statement","Then3Statement"
+// "Given1Value","Given2Value","Given3Value",,"Then1Value","Then2Value","Then3Value"
+
+//example whenStatement: 
+//      "When I try to fit the shape in the hole"
+// example csv:
+//      the shape,the colour,the hole,,shape fits,the colour
+//      a square, red, a square,,true, irrelevant
+//      a square,red,a circle,, false, irrelevant
+//      a square, red, a triangle,,false, irrelevant
+//      ...
+
+// example output:
+//      Given the shape is a square
+//          And the colour is red
+//          And the hole is a square
+//          When I try to fit the shape in the hole
+//          Then the shape fits is true
+//          And the colour is irrelevant
+
+namespace TruthTableToGherkinTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            var csvFilename = @"C:\test\testfile.csv";
+
+            var whenStatement = "When I try to fit the shape in the hole";            
+
+            using (CsvReader csv = new CsvReader(new StreamReader(csvFilename), true))
+            {
+                int fieldCount = csv.FieldCount;
+                string[] headers = csv.GetFieldHeaders();
+
+                var outputLines = new List<string>();
+
+                while (csv.ReadNextRecord())
+                {
+                    var colIndex = 0;
+                    outputLines.Add(string.Format("Given {0} is {1}", headers[colIndex], csv[colIndex]));
+                    colIndex++;
+
+                    while (csv[colIndex] != "")
+                    {
+                        outputLines.Add(string.Format("    And {0} is {1}", headers[colIndex], csv[colIndex]));
+                        colIndex++;
+                    }
+
+                    outputLines.Add(string.Format("    {0}", whenStatement));
+                    colIndex++;
+
+                    outputLines.Add(string.Format("    Then {0} is {1}", headers[colIndex], csv[colIndex]));
+                    colIndex++;
+
+                    while (colIndex < fieldCount)
+                    {
+                        outputLines.Add(string.Format("    And {0} is {1}", headers[colIndex], csv[colIndex]));
+                        colIndex++;
+                    }
+
+                    outputLines.Add("");
+                }
+
+                System.IO.File.WriteAllLines(@"C:\test\gherkinTests.txt", outputLines);
+            }
+        }
+    }
+}
